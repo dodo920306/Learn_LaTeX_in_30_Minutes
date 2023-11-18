aeb6bc991d13fda9c879797207a992cfe63ad827
Kirin Chu <74814435+dodo920306@users.noreply.github.com>
2023-11-18 16:53:04 +0800

    Update main.sh
    Adjust bibtex uses.

--- main.sh
+++ main.sh
@@ -3,13 +3,18 @@
 if [[ $1 == "all" ]]; then
     for file in *.tex; do
         xelatex $file
-        # twice for ones including images.
+        # for .bib
+        bibtex ${file%.tex}.aux
+        xelatex $file
+        # Third pass
         xelatex $file
     done
 elif [[ $1 == "clear" ]]; then
     rm -f *.pdf
 else
     xelatex $1
+    bibtex ${1%.tex}.aux
+    xelatex $1
     xelatex $1
 fi
-rm -f *.aux *.log *.toc *.out
+rm -f *.aux *.log *.toc *.out *.bbl *.blg
