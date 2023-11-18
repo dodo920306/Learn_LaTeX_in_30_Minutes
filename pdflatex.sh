511605dc6535bfed036e4355befcd683f8635573
Kirin Chu <74814435+dodo920306@users.noreply.github.com>
2023-11-18 22:18:40 +0800

    Update pdflatex.sh
    Third pass is added for special need.

--- pdflatex.sh
+++ pdflatex.sh
@@ -3,10 +3,16 @@
 if [[ $1 == "all" ]]; then
     for file in *.tex; do
         pdflatex $file
+        bibtex ${file%.tex}.aux
+        pdflatex $file
+        pdflatex $file
     done
 elif [[ $1 == "clear" ]]; then
     rm -f *.pdf
 else
+    pdflatex $1
+    bibtex ${1%.tex}.aux
+    pdflatex $1
     pdflatex $1
 fi
 rm -f *.aux *.log *.toc *.out *.bbl *.blg
